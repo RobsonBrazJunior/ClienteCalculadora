35a5e518de729955c8a3c38e94e5f33385c6a7f0
Junior Braz <robsonbrazjunior@gmail.com>
2018-09-03 21:34:55 -0400

    Criado instâncias de ServicoCalculadora na classe Principal

--- src/aplicacao/Principal.java
+++ src/aplicacao/Principal.java
@@ -1,9 +1,13 @@
 package aplicacao;
 
+import clientews.ServicoCalculadora;
+import clientews.ServicoCalculadora_Service;
+
 public class Principal {
 
     public static void main(String[] args) {
-    
-    }
-    
+        ServicoCalculadora_Service service = new ServicoCalculadora_Service();
+        ServicoCalculadora port = service.getServicoCalculadoraPort();
+        System.out.println("Soma = " + port.somar(20, 30));
+    }    
 }
